d07acc97373c908105d0aef76f352132bf641ffe
Anoop Remanan Syamala <53828695+anooprs@users.noreply.github.com>
2021-07-01 18:46:48 +0530

    Create plot2.R

---
+++ Exploratory Data Analysis/plot2.R
@@ -0,0 +1,24 @@
+## Load the dataset
+
+data_power <- read.csv("household_power_consumption.txt", header=TRUE, sep=';',nrows=2075259, na.strings="?", 
+                      check.names=F, stringsAsFactors=FALSE, comment.char="", quote='\"')
+
+data_power$Date <- as.Date(data_power$Date, format="%d/%m/%Y")
+
+## Subset data
+
+data <- subset(data_power, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+## Convert date formats
+
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Generate plot2.png
+
+plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+## Save the file
+
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
