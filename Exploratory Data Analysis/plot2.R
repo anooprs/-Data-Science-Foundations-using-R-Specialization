17ef14e9c263bc3549fdc20aa16e2b2e30966bdf
Anoop Remanan Syamala <53828695+anooprs@users.noreply.github.com>
2021-07-01 18:49:55 +0530

    Update plot2.R

--- Exploratory Data Analysis/plot2.R
+++ Exploratory Data Analysis/plot2.R
@@ -20,5 +20,5 @@ plot(data$Global_active_power~data$Datetime, type="l", ylab="Global Active Power
 
 ## Save the file
 
-dev.copy(png, file="plot1.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
