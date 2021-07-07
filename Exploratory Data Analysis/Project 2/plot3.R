d1faf6bb9cff6f152e80397fa1c70375e418b15f
Anoop Remanan Syamala <53828695+anooprs@users.noreply.github.com>
2021-07-07 14:34:41 +0530

    Create plot3.R

---
+++ Exploratory Data Analysis/Project 2/plot3.R
@@ -0,0 +1,13 @@
+#Question 3: Of the four types of sources indicated by the 𝚝𝚢𝚙𝚎 (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question?
+
+baltimore_NEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(baltimore_NEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") + theme_minimal() + 
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+dev.off()
