a7470ed4ddbcd014ee1568277e724a6f1b6e26c9
Anoop Remanan Syamala <53828695+anooprs@users.noreply.github.com>
2021-07-07 14:38:33 +0530

    Update plot5.R

--- Exploratory Data Analysis/Project 2/plot5.R
+++ Exploratory Data Analysis/Project 2/plot5.R
@@ -7,13 +7,11 @@ vehicles_NEI <- NEI[NEI[, SCC] %in% vehicles_SCC,]
 # Subset the vehicles NEI data to Baltimore's fip
 baltimoreVehicles_NEI <- vehicles_NEI[fips=="24510",]
 
-png("plot5.png"
+png("plot5.png")
 
 ggplot(baltimoreVehicles_NEI,aes(factor(year),Emissions)) +
   geom_bar(stat="identity", fill ="#FF9999" ,width=0.75) + theme_minimal() +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
-
-)
 
 dev.off()
